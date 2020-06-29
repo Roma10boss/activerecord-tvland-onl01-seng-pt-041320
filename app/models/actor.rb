1295d33ad5a362a376a2d269622454f36ce88214
Romario Gustave <romagustave10@gmail.com>
2020-06-29 21:15:40 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,11 +2,10 @@ class Actor < ActiveRecord::Base
    has_many :characters
   has_many :shows, through: :characters
 
-  def change
-    create_table :actors do |t|
-      t.string :first_name
-      t.string :last_name
-    end
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
 
   def list_roles
     roles = []
@@ -16,6 +15,5 @@ class Actor < ActiveRecord::Base
     roles << role.join(" - ")
     roles
   end
-end 
 
 end
