d21cbf24f8b2d1dbdfd647057b1616379dcb4c9a
Romario Gustave <romagustave10@gmail.com>
2020-06-29 21:18:43 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,8 +6,7 @@ class Show < ActiveRecord::Base
   
      def change
     create_table :actors_list do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :full_name
     end
   end
 end 
