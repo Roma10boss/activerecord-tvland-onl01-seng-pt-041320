cede929698ca2e38a627b1bce75cdf06971dad46
Romario Gustave <romagustave10@gmail.com>
2020-06-29 21:18:17 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,12 +3,11 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :network
   
-  def change 
+  
      def change
     create_table :actors_list do |t|
       t.string :first_name
       t.string :last_name
     end
   end
-end
 end 
